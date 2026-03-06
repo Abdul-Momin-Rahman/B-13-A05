7094a36be3e85d3bd5c58f62d600d2f109b5ff84
Abdul-Momin-Rahman <a.mominatick@gmail.com>
2026-03-06 22:22:58 +0600

    added login logic

---
+++ script.js
@@ -0,0 +1,12 @@
+const loginVerify = () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username == "admin" && password == "admin123") {
+        window.location.href = "homepage.html";
+    }
+
+    else{
+        alert('wrong username or password!')
+    }
+}
